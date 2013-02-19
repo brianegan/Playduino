9bdc4e338b8516fe51c9c977759727ebeea5669d
suziam <suz.hinton@gmail.com>
2013-02-18 17:13:32 -0800

    major cleanup, now ready for public\!

--- serialRead.js
+++ serialRead.js
@@ -2,10 +2,11 @@ var sys = require('util');
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
 
-var sp = new SerialPort("/dev/cu.usbmodem621", { 
+var sp = new SerialPort("/dev/cu.usbmodem1411", { 
   parser: serialport.parsers.readline("\n")
 });
 
+
 sp.on("data", function (data) {
-  sys.puts("CATS: "+data);
+  //sys.puts("CATS: "+data);
 });
