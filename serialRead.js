6c7c22c155c4d836f892f296a85d1d44ee49c286
suziam <suz.hinton@gmail.com>
2013-02-18 18:49:54 -0800

    added docs to serialRead.js

--- serialRead.js
+++ serialRead.js
@@ -1,3 +1,21 @@
+/*
+    _____                               
+   (, /   ) /)          /)     ,        
+    _/__ / // _       _(/       __   ___
+    /     (/_(_(_(_/_(_(_(_(__(_/ (_(_) 
+ ) /            .-/                     
+(_/            (_/                      
+
+*/
+
+/* 
+**
+this file not necessary, but a good way to test 
+that the Arduino is talking to the serial port properly 
+run it via node after connecting our Arduino
+**
+*/
+
 var sys = require('util');
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
@@ -8,5 +26,5 @@ var sp = new SerialPort("/dev/cu.usbmodem1411", {
 
 
 sp.on("data", function (data) {
-  //sys.puts("CATS: "+data);
+  sys.puts("CATS: "+data);
 });
